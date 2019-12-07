49e3d9fdd8c7b56fd17fb9696dfa9b7cc017d59f
Gabriel Burning <7988867+gburning@users.noreply.github.com>
2019-12-07 14:16:54 +0100

    project init, working initial version

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import App from './app.html'
+import { Store } from 'svelte/store.js';
+
+const store = new Store({
+	mapWidth: 0,
+	mapHeight: 0,
+	mapScale: 100,
+	mapFitWidth: true,
+	mapFitHeight: false,
+	mapLoaded: false,
+	gridSquaresPerRow: 10,
+	gridEnabled: true,
+	gridColor: '#ffffff',
+	gridOpacity: 50,
+	fogEnabled: true,
+	fogOpacity: 90,
+	fogBrushSize: 100,
+	fogBrushEffect: 'remove'
+});
+
+const app = new App({
+	target: document.body,
+	store
+})
+
+export default app
